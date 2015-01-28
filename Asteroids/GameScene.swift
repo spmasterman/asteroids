01c8fa57e3e153c0964541fb5fe95aeffdc20d0f
smasterman <shaun@masterman.com>
2015-01-28 00:24:54 +0000

    ship direction + thrust + bullets (no ship movement yet)

--- Asteroids/GameScene.swift
+++ Asteroids/GameScene.swift
@@ -10,48 +10,105 @@ import SpriteKit
 
 class GameScene: SKScene {
   
-    var ship = SKSpriteNode(imageNamed: "ship")
-    var joystick = Joystick(thumbNode: SKSpriteNode(imageNamed: "thumb_stick"), backdropNode:SKSpriteNode(imageNamed: "dpad"))
-   var fireButton = SKSpriteNode(imageNamed: "fire")
-   var thrustButton = SKSpriteNode(imageNamed: "thrust")
+  var ship = SKSpriteNode(imageNamed: "ship")
+  var thrust = SKSpriteNode(imageNamed: "ship_thrust")
+  var joystick = Joystick(thumbNode: SKSpriteNode(imageNamed: "thumb_stick"), backdropNode:SKSpriteNode(imageNamed: "dpad"))
+  var fireButton = SKSpriteNode(imageNamed: "fire")
+  var thrustButton = SKSpriteNode(imageNamed: "thrust")
+  var isThrusting = false
   
-    override func didMoveToView(view: SKView) {
-      backgroundColor = SKColor.blackColor()
-      ship.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
-      addChild(ship)
+  override func didMoveToView(view: SKView) {
+    backgroundColor = SKColor.blackColor()
+    ship.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+    addChild(ship)
+    setThrustPosition()
     
-      joystick.position = CGPointMake(CGFloat(100), CGFloat(100))
-      addChild(joystick)
+    addChild(thrust)
+    joystick.position = CGPointMake(CGFloat(100), CGFloat(100))
+    addChild(joystick)
+    
+    fireButton.position = CGPointMake(CGFloat(size.width - 100), CGFloat(100))
+    fireButton.zPosition = 1.0
+    fireButton.name = "fireButtonNode"
+    thrustButton.position = CGPointMake(CGFloat(size.width - 170), CGFloat(100))
+    thrustButton.zPosition = 1.0
+    thrustButton.name = "thrustButtonNode"
+    
+    addChild(fireButton)
+    addChild(thrustButton)
+  }
+  
+  func setThrustPosition() {
+    thrust.zRotation = ship.zRotation
+    thrust.hidden = !isThrusting
+    let bearing = getShipBearing()
+    
+    let x = ship.position.x - CGFloat(bearing.x) * ship.size.width/2.0
+    let y = ship.position.y - CGFloat(bearing.y) * ship.size.width/2.0
+    thrust.position = CGPointMake(x, y)
+
+  }
+  
+  func getShipBearing() -> Vector2
+  {
+     return Vector2(Scalar(ship.size.width) / 2.0, 0).rotatedBy(Scalar(ship.zRotation)).normalized()
+  }
+  
+  override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+    for touch: AnyObject in touches {
+      let location = touch.locationInNode(self)
+      let bearing = getShipBearing()
       
-      fireButton.position = CGPointMake(CGFloat(size.width - 100), CGFloat(100))
-      thrustButton.position = CGPointMake(CGFloat(size.width - 170), CGFloat(100))
-      addChild(fireButton)
-      addChild(thrustButton)
-    }
-    
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"asteroid_large_1")
-            
-            sprite.xScale = 1
-            sprite.yScale = 1
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI / 3), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-  
-    override func update(currentTime: CFTimeInterval) {
-      if joystick.velocity.x != 0 || joystick.velocity.y != 0 {
-        ship.zRotation = joystick.angularVelocity + CGFloat(M_PI_2)
+      var node = nodeAtPoint(location)
+      
+      if (node.name == "fireButtonNode") {
+        let bullet = SKSpriteNode(imageNamed:"shot")
+        bullet.xScale = 2
+        bullet.yScale = 2
+        bullet.name = "bulletNode"
+
+        let x = ship.position.x + CGFloat(bearing.x) * ship.size.width/2.0
+        let y = ship.position.y + CGFloat(bearing.y) * ship.size.width/2.0
+        bullet.position = CGPointMake(x, y)
+
+        let bulletSpeed = CGFloat(30.0)
+        let action = SKAction.moveByX(CGFloat(bearing.x) * bulletSpeed, y: CGFloat(bearing.y) * bulletSpeed, duration: 1)
+        bullet.runAction(SKAction.repeatActionForever(action))
+        self.addChild(bullet)
+      }
+      
+      if (node.name == "thrustButtonNode") {
+        isThrusting = true
       }
     }
+  }
+  
+  override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+    for touch: AnyObject in touches {
+      let location = touch.locationInNode(self)
+      var node = nodeAtPoint(location)
+      if (node.name == "thrustButtonNode") {
+        isThrusting = false
+      }
+    }
+  }
+  
+  override func update(currentTime: CFTimeInterval) {
+    if joystick.velocity.x != 0 || joystick.velocity.y != 0 {
+      ship.zRotation = joystick.angularVelocity + CGFloat(M_PI_2)
+    }
+    setThrustPosition()
+    
+    // remove bullets that leave screen
+    self.enumerateChildNodesWithName("bulletNode", usingBlock:  {
+      (node: SKNode!, stop: UnsafeMutablePointer <ObjCBool>) -> Void in
+      
+      if (node.position.x < 0 || node.position.x > self.size.width || node.position.y < 0 || node.position.y > self.size.height) {
+        node.removeFromParent()
+        println("removed!")
+      }
+      
+    })
+    
+  }
 }
---
+++ Asteroids/VectorMath.swift
@@ -0,0 +1,156 @@
+//
+//  VectorMath.swift
+//  Asteroids
+//
+//  Created by Shaun Masterman on 27/01/2015.
+//  Copyright (c) 2015 Southbase Limited. All rights reserved.
+//
+
+import Foundation
+
+typealias Scalar = Float
+
+struct Vector2 {
+  var x: Scalar
+  var y: Scalar
+}
+
+//MARK: Scalar
+
+extension Scalar {
+  
+  static let Pi = Scalar(M_PI)
+  static let HalfPi = Scalar(M_PI_2)
+  static let QuarterPi = Scalar(M_PI_4)
+  static let TwoPi = Scalar(M_PI * 2)
+  static let DegreesPerRadian = 180 / Pi
+  static let RadiansPerDegree = Pi / 180
+  static let Epsilon: Scalar = 0.0001
+}
+
+func ~=(lhs: Scalar, rhs: Scalar) -> Bool {
+  return abs(lhs - rhs) < .Epsilon
+}
+
+//MARK: Vector2
+
+extension Vector2: Equatable, Hashable {
+  
+  static let Zero = Vector2(0, 0)
+  static let X = Vector2(1, 0)
+  static let Y = Vector2(0, 1)
+  
+  var hashValue: Int {
+    return x.hashValue &+ y.hashValue
+  }
+  
+  var lengthSquared: Scalar {
+    return x * x + y * y
+  }
+  
+  var length: Scalar {
+    return sqrt(lengthSquared)
+  }
+  
+  var inverse: Vector2 {
+    return -self
+  }
+  
+  init(_ x: Scalar, _ y: Scalar) {
+    self.init(x: x, y: y)
+  }
+  
+  init(_ v: [Scalar]) {
+    
+    assert(v.count == 2, "array must contain 2 elements, contained \(v.count)")
+    
+    x = v[0]
+    y = v[1]
+  }
+  
+  func toArray() -> [Scalar] {
+    return [x, y]
+  }
+  
+  func dot(v: Vector2) -> Scalar {
+    return x * v.x + y * v.y
+  }
+  
+  func cross(v: Vector2) -> Scalar {
+    return x * v.y - y * v.x
+  }
+  
+  func normalized() -> Vector2 {
+    
+    let lengthSquared = self.lengthSquared
+    if lengthSquared ~= 0 || lengthSquared ~= 1 {
+      return self
+    }
+    return self / sqrt(lengthSquared)
+  }
+  
+  func rotatedBy(radians: Scalar) -> Vector2 {
+    
+    let cs = cos(radians)
+    let sn = sin(radians)
+    return Vector2(x * cs - y * sn, x * sn + y * cs)
+  }
+  
+  func rotatedBy(radians: Scalar, around pivot: Vector2) -> Vector2 {
+    return (self - pivot).rotatedBy(radians) + pivot
+  }
+  
+  func angleWith(v: Vector2) -> Scalar {
+    
+    if self == v {
+      return 0
+    }
+    
+    let t1 = normalized()
+    let t2 = v.normalized()
+    let cross = t1.cross(t2)
+    let dot = max(-1, min(1, t1.dot(t2)))
+    
+    return atan2(cross, dot)
+  }
+  
+  func interpolatedWith(v: Vector2, t: Scalar) -> Vector2 {
+    return self + (v - self) * t
+  }
+}
+
+prefix func -(v: Vector2) -> Vector2 {
+  return Vector2(-v.x, -v.y)
+}
+
+func +(lhs: Vector2, rhs: Vector2) -> Vector2 {
+  return Vector2(lhs.x + rhs.x, lhs.y + rhs.y)
+}
+
+func -(lhs: Vector2, rhs: Vector2) -> Vector2 {
+  return Vector2(lhs.x - rhs.x, lhs.y - rhs.y)
+}
+
+func *(lhs: Vector2, rhs: Vector2) -> Vector2 {
+  return Vector2(lhs.x * rhs.x, lhs.y * rhs.y)
+}
+
+func *(lhs: Vector2, rhs: Scalar) -> Vector2 {
+  return Vector2(lhs.x * rhs, lhs.y * rhs)
+}
+
+func /(lhs: Vector2, rhs: Vector2) -> Vector2 {
+  return Vector2(lhs.x / rhs.x, lhs.y / rhs.y)
+}
+
+func /(lhs: Vector2, rhs: Scalar) -> Vector2 {
+  return Vector2(lhs.x / rhs, lhs.y / rhs)
+}
+
+func ==(lhs: Vector2, rhs: Vector2) -> Bool {
+  return lhs.x == rhs.x && lhs.y == rhs.y
+}
+
+func ~=(lhs: Vector2, rhs: Vector2) -> Bool {
+  return lhs.x ~= rhs.x && lhs.y ~= rhs.y
+}
